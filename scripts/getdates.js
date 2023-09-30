4d22d95e9dfb6f1df210c42abcb3c5e0b1995f5f
princeuche-project <princeuche1996@outlook.com>
2023-10-01 02:05:08 +0400

    added file

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,12 @@
 let lastModif = new Date()
 document.getElementById("lastModified").innerHTML = lastModif
 
+const hamp= document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamp.addEventListener('click', () => {
+	navigation.classList.toggle('show');
+	hamp.classList.toggle('show');
+});
+
 
