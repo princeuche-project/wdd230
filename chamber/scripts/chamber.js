f12c88f011b94b12a9f60dfb45985b68013abcb4
princeuche-project <princeuche1996@outlook.com>
2023-10-08 20:55:27 +0400

    added file

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,8 +1,15 @@
-function myFunction() {
-    let x = document.getElementById("myNav");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
+// function myFunction() {
+//     let x = document.getElementById("myNav");
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//     } else {
+//       x.style.display = "block";
+//     }
+//   }
+const hamp = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation-bar');
+
+hamp.addEventListener('click', () => {
+  navigation.classList.toggle('show');
+  hamp.classList.toggle('show');
+});
