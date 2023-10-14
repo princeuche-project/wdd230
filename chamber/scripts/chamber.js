6902f798acc7461d7b9155d17a70d1fff88490de
princeuche-project <princeuche1996@outlook.com>
2023-10-15 00:03:22 +0400

    added file

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -9,3 +9,13 @@ hamp.addEventListener('click', () => {
   navigation.classList.toggle('show');
   hamp.classList.toggle('show');
 });
+
+
+
+const themeToggler = document.querySelector(".theme-toggler")
+
+themeToggler.addEventListener("click", () => {
+  document.body.classList.toggle("lemmon");
+  themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+  themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
+})
