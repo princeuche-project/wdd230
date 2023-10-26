5854c43b84f746da5b9ad6d0bda4b35505d337b2
princeuche-project <princeuche1996@outlook.com>
2023-10-26 11:19:09 +0400

    added file

--- scripts/lastVisited.js
+++ scripts/lastVisited.js
@@ -1,23 +1,8 @@
-// let lastVisited = document.getElementById("lastVisited").value;
-
-// let numOfVisit = Number(window.localStorage.getItem("numVisit")) || 0;
-
-// if (numOfVisit !== null) {
-//     lastVisited = numOfVisit;
-// } else {
-//     lastVisited = `This is your first visit. Welcome!`;
-// }
-// numOfVisit++;
-
-// localStorage.setItem("numVisite", numOfVisit);
-
-// document.getElementById("output").innerHTML = lastVisited
-
 let lastVisited = document.getElementById("lastVisited").value;
 
 let numOfVisit = Number(window.localStorage.getItem("numOfVisits")) || 0;
 
-if (numOfVisit !== null) {
+if (numOfVisit !== 0) {
     lastVisited = numOfVisit;
 } else {
     lastVisited = `This is your first visit. 🥳 Welcome!`;
