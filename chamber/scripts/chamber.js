4a9757ae4fb5f231af8e6af219416c0d6f8fc27f
princeuche-project <princeuche1996@outlook.com>
2023-10-30 02:22:16 +0400

    added file

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -2,17 +2,18 @@ let lastvisit = new Date(document.lastModified);
 document.getElementById("lastvisit").innerHTML = lastvisit;
 
 
-const hamp = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation-bar');
+// const hamp = document.querySelector('#menu');
+// const navigation = document.querySelector('.navigation-bar');
 
-hamp.addEventListener('click', () => {
-  navigation.classList.toggle('show');
-  hamp.classList.toggle('show');
-});
+// hamp.addEventListener('click', () => {
+//   navigation.classList.toggle('show');
+//   hamp.classList.toggle('show');
+// });
 
 
 
 const themeToggler = document.querySelector(".theme-toggler")
+
 
 themeToggler.addEventListener("click", () => {
   document.body.classList.toggle("lemmon");
