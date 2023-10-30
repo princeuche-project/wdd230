76f8e0840e8dbf74cbf4600d4696045f1dccbc39
princeuche-project <princeuche1996@outlook.com>
2023-10-30 21:15:09 +0400

    added file

--- chamber/scripts/lastVisited.js
+++ chamber/scripts/lastVisited.js
@@ -1,7 +1,7 @@
 let now = new Date();
-let nowSame = Date.now();
+let msTodays = Date.now()
 let dayOfWeek = now.getDay();
-let sameday = nowSame
+let sameday = msTodays
 
 let lastVisited = document.getElementById("lastVisited").value;
 
@@ -9,7 +9,7 @@ let numOfVisit = Number(window.localStorage.getItem("numOfVisits")) || 0;
 
 if (numOfVisit !== 0 && dayOfWeek !== 0) {
     lastVisited = `You last visited ${numOfVisit} days ago.`
-} else if (sameday == dayOfWeek) {
+} else if (dayOfWeek > msTodays) {
     lastVisited = `Back so soon! Awesome`;
 } else {
     lastVisited = "Welcome! Lets us know if you have any questions.";
@@ -19,27 +19,3 @@ numOfVisit++;
 localStorage.setItem("numOfVisits", numOfVisit);
 
 document.getElementById("output").innerHTML = lastVisited
-
-
-
-
-
-
-// let month = now.getMonth();
-// let dayOfMonth = now.getDate();
-
-// // initialize the message
-// let message;
-// // Check if the days of the month is Holidays or days of the week to be weekends.
-// if (dayOfWeek == 0 && dayOfWeek == 6) {
-//     message = "Sleep In!"
-// } else if ((month == 0 && dayOfMonth == 1) ||
-//     (month == 6 && dayOfMonth == 4) ||
-//     (month == 11 && dayOfMonth == 25)) {
-//     message = "Sleep In!"
-// } else {
-//     message = "Get Up!"
-// }
-// // out put the message.
-// document.getElementById("output").innerHTML = message
-// }
