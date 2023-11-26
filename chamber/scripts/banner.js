b40bc6ad1bfd3be444611d8898faf7a83325bb1b
princeuche-project <princeuche1996@outlook.com>
2023-11-26 22:05:17 +0400

    added file

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -11,7 +11,7 @@ function showBanner() {
   let today = new Date();
   let day = today.getDay();
 
-  if (day === 1 || day === 3) {
+  if (day >= 1 || day <= 3) {
     banner.style.display = "block";
   }
 }
